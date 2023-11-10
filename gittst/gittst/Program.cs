f05daae8a3008c2b05839918d5303ca31279c683
HirokiYamane <yamane@comp.co.jp>
2023-11-10 10:22:09 +0900

    syokai

---
+++ gittst/gittst/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gittst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
